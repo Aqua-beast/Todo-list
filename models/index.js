4996547ac19cd06a276f0bef1356e173d273e89d
Aryan Sadh <85673066+Aqua-beast@users.noreply.github.com>
2023-05-10 19:54:09 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
-mongoose.connect( process.env.MONGODB_URI, {
-  // connecting to the mongodb database name: "todo-app" locally
-  keepAlive: true, // keeping the connection alive
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-mongoose.set("debug", true) // enabling debugging information to be printed to the console for debugging purposes
-mongoose.Promise = Promise // setting mongoose's Promise to use Node's Promise
+// const mongoose = require("mongoose")
+// mongoose.connect( process.env.MONGODB_URI, {
+//   // connecting to the mongodb database name: "todo-app" locally
+//   keepAlive: true, // keeping the connection alive
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// mongoose.set("debug", true) // enabling debugging information to be printed to the console for debugging purposes
+// mongoose.Promise = Promise // setting mongoose's Promise to use Node's Promise
 
 
-module.exports.Todo = require("./todo") // requiring the todo model that we just created in mongodb
+// module.exports.Todo = require("./todo") // requiring the todo model that we just created in mongodb
