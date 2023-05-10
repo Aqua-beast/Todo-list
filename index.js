8275a09334e544e52f2cd9c6cf7361cbbea111f5
Aryan Sadh <85673066+Aqua-beast@users.noreply.github.com>
2023-05-10 19:53:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,91 @@
-const express = require("express")
-const app = express() // create express app
-const bodyParser = require("body-parser")
-const PORT = process.env.PORT || 3000 // set port to listen on
-const db = require("./models/") // import the Todo model from ./models
-const path = require('path'); // import path module
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 
-// // set view engine to EJS
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+// import the Todo model from the other file
+const Todo = require("./models/todo");
 
-app.use(bodyParser.json()) // parse incoming request data as JSON
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// helper function to return a successful response
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+
+app.use(bodyParser.json());
+
 function success(res, payload) {
-  return res.status(200).json(payload)
+  return res.status(200).json(payload);
 }
 
-// route to get all todos from the database and return them as JSON
 app.get("/todos", async (req, res, next) => {
   try {
-    const todos = await db.Todo.find({})
-    return success(res, todos)
+    // use the Todo model to retrieve all todos from the database
+    const todos = await Todo.find({});
+    return success(res, todos);
   } catch (err) {
-    next({ status: 400, message: "failed to get todos" })
+    next({ status: 400, message: "failed to get todos" });
   }
-})
+});
 
-// route to create a new todo and return it as JSON
 app.post("/todos", async (req, res, next) => {
   try {
-    const todo = await db.Todo.create(req.body)
-    return success(res, todo)
+    const todo = await Todo.create(req.body);
+    return success(res, todo);
   } catch (err) {
-    next({ status: 400, message: "failed to create todo" })
+    next({ status: 400, message: "failed to create todo" });
   }
-})
+});
 
-// route to update an existing todo and return it as JSON
 app.put("/todos/:id", async (req, res, next) => {
   try {
-    const todo = await db.Todo.findByIdAndUpdate(req.params.id, req.body, {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-    })
-    return success(res, todo)
+    });
+    return success(res, todo);
   } catch (err) {
-    next({ status: 400, message: "failed to update todo" })
+    next({ status: 400, message: "failed to update todo" });
   }
-})
+});
 
-// route to delete an existing todo and return a success message as JSON
 app.delete("/todos/:id", async (req, res, next) => {
   try {
-    await db.Todo.findByIdAndRemove(req.params.id)
-    return success(res, "todo deleted!")
+    await Todo.findByIdAndRemove(req.params.id);
+    return success(res, "todo deleted!");
   } catch (err) {
-    next({ status: 400, message: "failed to delete todo" })
+    next({ status: 400, message: "failed to delete todo" });
   }
-})
+});
 
-// route for pending tasks display
 app.get("/todos/pending", async (req, res, next) => {
   try {
-    const pendingTodos = await db.Todo.find({ completed: false })
-    return success(res, pendingTodos)
+    const pendingTodos = await Todo.find({ completed: false });
+    return success(res, pendingTodos);
   } catch (err) {
-    next({ status: 400, message: "failed to get pending todos" })
+    next({ status: 400, message: "failed to get pending todos" });
   }
-})
+});
 
-// route for completed tasks display
 app.get("/todos/completed", async (req, res, next) => {
   try {
-    const completedTodos = await db.Todo.find({ completed: true })
-    return success(res, completedTodos)
+    const completedTodos = await Todo.find({ completed: true });
+    return success(res, completedTodos);
   } catch (err) {
-    next({ status: 400, message: "failed to get completed todos" })
+    next({ status: 400, message: "failed to get completed todos" });
   }
-})
+});
 
-// error handling middleware to return a JSON response with error details
 app.use((err, req, res, next) => {
   return res.status(err.status || 400).json({
     status: err.status || 400,
     message: err.message || "there was an error processing request",
-  })
-})
+  });
+});
 
-// start listening on the specified port
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
